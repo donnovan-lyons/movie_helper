c8ef9dc8196d271e33b2becfb75441fa4823ffc8
dlyons8 <dlyons8@binghamton.edu>
2018-10-17 20:46:54 +0300

    first commit

---
+++ lib/movie_helper.rb
@@ -0,0 +1,5 @@
+require "movie_helper/version"
+
+module MovieHelper
+  # Your code goes here...
+end
---
+++ spec/movie_helper_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe MovieHelper do
+  it "has a version number" do
+    expect(MovieHelper::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
